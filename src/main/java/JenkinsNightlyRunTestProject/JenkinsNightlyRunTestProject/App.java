9d985a5dd9415f9d1a38e5bdb8d6ddf5ce34dd73
khaleelsafin <kkhaleel043@gmail.com>
2025-05-24 11:33:32 +0530

    First commit

---
+++ src/main/java/JenkinsNightlyRunTestProject/JenkinsNightlyRunTestProject/App.java
@@ -0,0 +1,13 @@
+package JenkinsNightlyRunTestProject.JenkinsNightlyRunTestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
